fdda75c1112e76dd8633fb8df0ca250117a6f989
George Taylor <gtaylor28@outlook.com>
2026-08-23 18:10:40 +0100

    Added greater range of angles to iterate over. Updated feeding script accordingly to match swimming. Used updated microswimmers package to fix results.

--- AntFeedingSim.jl
+++ AntFeedingSim.jl
@@ -6,25 +6,6 @@ using FastGaussQuadrature
 using Statistics
 include("excavate_body_design.jl")
 
-#Posterior flagellum
-f = PlanarStandingWaveFlagellum{Float64}(10.0, 6.283185307179586*5, 0.0, [0.15, 0.0, -0.35, 0.0], [-0.3, 0.4, 0.0, -0.3])
-
-posterior = PlanarVanedFlagellum(f, 0.1, 0.6, .7)
-
-##excavate body
-# jakoba parameters
-el = SuperEllipsoid(3.9, 2.2, 2.2)
-groove = Posed(SuperEllipsoid(3.9, 2.2, 2.2; κx = 0.1, κy = 0.15), Frame([0., 0., 0.85], MicroSwimmers.I3))
-body = ImplicitExcavateBody(el, groove, 50.0) 
-# jakoba_pars = (a = 3.9, b = 2.2, c = 2.2, a_g = 3.9, b_g = 2.2, c_g = 2.2, p_a = 2, p_b = 2, p_c = 2, z_s = 0.85, θ = 0.0, κ_x = 0.1, κ_y = 0.15)
-excavate_body_tool(body)
-
-#Static curvature
-C = 0.0
-
-#Radius
-r = 3.9
-
 function velocity_flux_polar2(u, x0, y0, z, R; Nr=20, Nθ=20)
     rs_raw, wrs = gausslegendre(Nr)
     θs_raw, wθs = gausslegendre(Nθ)
@@ -49,58 +30,93 @@ function velocity_flux_polar2(u, x0, y0, z, R; Nr=20, Nθ=20)
     total_flux
 end
 
-#All curvatures
-curvs = []
+
+#Posterior flagellum
+f = PlanarStandingWaveFlagellum{Float64}(10.0, 6.283185307179586, 0.0, [0.15, 0.0, -0.35, 0.0], [-0.3, 0.4, 0.0, -0.3])
+
+posterior = PlanarVanedFlagellum(f, 0.1, 0.6, .7)
+
+anterior = ThreeDimensionalFlagellum{Float64}(9.0, 1.0, 0.0, 1.16, 14.0, 0.16, 1.0, 0.8, 0.53, 21.0, -0.16, 0.0, 0.3584073464102069)
+
+
+##excavate body
+# jakoba parameters
+el = SuperEllipsoid(3.9, 2.2, 2.2)
+groove = Posed(SuperEllipsoid(3.9, 2.2, 2.2; κx = 0.1, κy = 0.15), Frame([0., 0., 0.85], MicroSwimmers.I3))
+body = ImplicitExcavateBody(el, groove, 50.0) 
+# jakoba_pars = (a = 3.9, b = 2.2, c = 2.2, a_g = 3.9, b_g = 2.2, c_g = 2.2, p_a = 2, p_b = 2, p_c = 2, z_s = 0.85, θ = 0.0, κ_x = 0.1, κ_y = 0.15)
+
+#Radius
+r = 3.9
+
+#azimuthal curvature
+azuCurvs = collect(-pi/4:pi/32:pi/4)
+
+#elevation curvatures
+eleCurvs = collect(-pi/4:pi/32:pi/4)
 
 #All Effeciencys
-eff = []
+eff = zeros(17,17)
 
-for i in range(0,stop = pi/4, step = pi/16)
-    anterior = ThreeDimensionalFlagellum(9., 0.0, 1.25, 0.1, 12.5, C - i, 1.0, 1.25, 0.1, 12.5, 0., 0., 0.)
-    # anterior = ThreeDimensionalFlagellum{Float64}(9.0, 1.0, 0.0, 1.16, 14.0, C - i, 1.0, 0.8, 0.53, 21.0, -0.16, 0.0, 0.3584073464102069)
+for azi in range(start = -pi/4, stop = pi/4, step = pi/32)
+    anterior.Cᵩ = azi
+    #Gets correct index (1 indexing not 0)
+    col = round(Int,(azi / (pi/32)+9))
+    for elv in range(start = -pi/4, stop = pi/4, step = pi/32)
+        anterior.C_θ = elv
+        
+        row = round(Int,(elv / (pi/32)+9))
 
-    excavate = MicroSwimmer([
-        Part(body, 313, 3117),
-        Part(posterior, 31, 117; location=[-3.7, 0.0, 0.25],orientation=rotation_matrix([0.0, 1.0, 0.0], -π/36)),
-        Part(anterior, 31, 117; location=[-3.9, 0., 0.25],orientation=rotation_matrix([0, 1.0, 0.0], -2π/3) )
-    ])
-    # animate(excavate)
+        anterior_part = Part(anterior, 31, 117; location=[-3.9, 0., 0.25],orientation=rotation_matrix([0, 1.0, 0.0], -2π/3) )
 
-    # flux and power
-    rprob = ResistanceProblem(excavate, eps=0.1, wall=true)
-    u = FluidVelocity(rprob)
+        excavate = MicroSwimmer([
+            Part(body, 313, 3117),
+            Part(posterior, 31, 117; location=[-3.7, 0.0, 0.25],orientation=rotation_matrix([0.0, 1.0, 0.0], -π/36)),
+            anterior_part
+        ])
+        # animate(excavate)
 
-    println(total_power(rprob))
-    
-    fluxes = []
-    powers = []
-    for t in range(0,1,10)[1:end-1]
-        update_boundary!(rprob, t)
-        solve_problem!(rprob)
-        @info "" fluxes
-        @info "" powers
-        push!(fluxes, velocity_flux_polar2(u, 0.0, 0.0, 25, r))
-        push!(powers, total_power(rprob))
-    end     
-    flux = mean(fluxes)
-    power = mean(powers)
+        #Initialise swimming problem 
+        rprob = ResistanceProblem(excavate, eps=0.1, wall=true)
 
-    e = (flux^2)  / power
+        # Update Problem
+        # update_boundary!(excavate,0.0)
+        #Get fluid velocity
+        u = FluidVelocity(rprob)
+        
+        fluxes = []
+        powers = []
+        for t in range(0,1,10)[1:end-1]
+            update_boundary!(rprob, t)
+            solve_problem!(rprob)
+            push!(fluxes, velocity_flux_polar2(u, 0.0, 0.0, 25, r))
+            push!(powers, total_power(rprob))
+        end     
+        flux = mean(fluxes)
+        power = mean(powers)
 
-    #Effeciency
-    println("Effeciency: $e")
-    println("Power}: $power")
+        e = (flux^2)  / power
 
-    #update arrays
-    push!(eff,e)
-    push!(curvs,C-i)
+        # #Effeciency
+        println("Effeciency: $e")
+        # println("Power}: $power")
+
+        #update Effeciency matrix
+        eff[row,col] = e
+        
+    end
 end
 
 fig = Figure()
 ax = Axis(fig[1,1],
-    xlabel = "Curvature",
-    ylabel = "Effeciency"
+    xlabel = "Azimuthal curvature",
+    ylabel = "Elevation curvature",
 )
 
-lines!(ax,curvs,eff)
+
+hm = heatmap!(ax,azuCurvs,eleCurvs,vels)
+
+Colorbar(fig[1,2],hm,label = "velocity (μm/beat)")
+
+save("curvatureFeedingHEAT.png",fig)
 
--- AntSwimSim.jl
+++ AntSwimSim.jl
@@ -7,7 +7,7 @@ using Statistics
 include("excavate_body_design.jl")
 
 #Posterior flagellum
-f = PlanarStandingWaveFlagellum{Float64}(10.0, 6.283185307179586*5, 0.0, [0.15, 0.0, -0.35, 0.0], [-0.3, 0.4, 0.0, -0.3])
+f = PlanarStandingWaveFlagellum{Float64}(10.0, 6.283185307179586, 0.0, [0.15, 0.0, -0.35, 0.0], [-0.3, 0.4, 0.0, -0.3])
 
 posterior = PlanarVanedFlagellum(f, 0.1, 0.6, .7)
 
@@ -20,25 +20,35 @@ body = ImplicitExcavateBody(el, groove, 50.0)
 # excavate_body_tool(body)
 
 #azimuthal curvature
-azuCurvs = collect(0:pi/16:pi/4)
+azuCurvs = collect(-pi/4:pi/32:pi/4)
 
 #elevation curvatures
-eleCurvs = collect(0:pi/16:pi/4)
-
-println(eleCurvs)
+eleCurvs = collect(-pi/4:pi/32:pi/4)
 
 #All Velocities matrix
-vels = zeros(5,5)
+vels = zeros(17,17)
 
-anterior = ThreeDimensionalFlagellum(9., 0., 1.25, 0.1, 12.5, C, 1.0, 1.25, 0.1, 12.5, 0., 0., 0.)
-# anterior = ThreeDimensionalFlagellum{Float64}(9.0, 1.0, 0.0, 1.16, 14.0, C - i, 1.0, 0.8, 0.53, 21.0, -0.16, 0.0, 0.3584073464102069)
+#Angular velocites
+angVels = zeros(17,17)
 
-anterior_part = Part(anterior, 31, 117; location=[-3.9, 0., 0],orientation=rotation_matrix([0, 1.0, 0.0], -2π/3) )
+#Torsion
+tor = zeros(17,17)
+
+#Polar Direction
+pol = zeros(17,17)
+
+#azimuthal Direction
+aziDir = zeros(17,17)
+
+# anterior = ThreeDimensionalFlagellum(9., 1.0, 1.25, 0.1, 12.5, 0., 1.0, 1.25, 0.1, 12.5, 0., 0., 0.)
+anterior = ThreeDimensionalFlagellum{Float64}(9.0, 1.0, 0.0, 1.16, 14.0, 0.16, 1.0, 0.8, 0.53, 21.0, -0.16, 0.0, 0.3584073464102069)
+
+anterior_part = Part(anterior, 31, 117; location=[-3.9, 0., 0.25],orientation=rotation_matrix([0, 1.0, 0.0], -2π/3) )
 
 excavate = MicroSwimmer([
     Part(body, 313, 3117),
     Part(posterior, 31, 117; location=[-3.7, 0.0, 0.25],orientation=rotation_matrix([0.0, 1.0, 0.0], -π/36)),
-    Part(anterior, 31, 117; location=[-3.9, 0., 0.25],orientation=rotation_matrix([0, 1.0, 0.0], -2π/3) )
+    anterior_part
 ])
 # animate(excavate)
 
@@ -47,11 +57,15 @@ prob = SwimmingTrajectoryProblem(excavate, eps=0.1, t_final=1.0, saveat=0.01)
 
 
 # #For loop to investigate 
-for azi in range(0, stop = pi/4, step = pi/16)
-    anterior.Cᵩ = - azi
-    for elv in range(0, stop = pi/4, step = pi/16)
-        anterior.C_θ = -elv
+for azi in range(start = -pi/4, stop = pi/4, step = pi/32)
+    anterior.Cᵩ = azi
+    #Gets correct index (1 indexing not 0)
+    col = round(Int,(azi / (pi/32)+9))
+    for elv in range(start = -pi/4, stop = pi/4, step = pi/32)
+        anterior.C_θ = elv
         
+        row = round(Int,(elv / (pi/32)+9))
+
         #update parameters
         update_boundary!(excavate,0.0)
 
@@ -64,8 +78,15 @@ for azi in range(0, stop = pi/4, step = pi/16)
         helix = fit_helix(traj)
 
         #Append Axis velocity
-        vels[Int8(elv/(pi/16))+1,Int8(azi/(pi/16))+1] = axis_velocity(helix)
-        #Append Current curvature
+        vels[row,col] = axis_velocity(helix)
+
+        angVels[row,col] = axis_angular_velocity(helix)
+
+        tor[row,col] = torsion(helix)
+
+        pol[row,col] = axis_polar_angle(helix)
+
+        aziDir[row,col] = axis_azimuthal_angle(helix)
     end
 end
 
@@ -74,13 +95,6 @@ end
 
 #Create Figure
 fig = Figure()
-ax = Axis(fig[1,1],
-    xlabel = "Curvature Angle (rad)",
-    ylabel = "Velocity (μm/beat)"
-)
-
-lines!(ax,curvs,vels)
-
 ax = Axis(fig[1,1],
     xlabel = "Azimuthal curvature",
     ylabel = "Elevation curvature",
@@ -93,3 +107,54 @@ Colorbar(fig[1,2],hm,label = "velocity (μm/beat)")
 
 save("curvatureANDVelocityHEAT.png",fig)
 
+fig = Figure()
+ax = Axis(fig[1,1],
+    xlabel = "Azimuthal curvature",
+    ylabel = "Elevation curvature",
+)
+
+
+hm = heatmap!(ax,azuCurvs,eleCurvs,angVels)
+
+Colorbar(fig[1,2],hm,label = "Angular Velocity (μm/beat)")
+
+save("curvatureANDAngVelocityHEAT.png",fig)
+
+fig = Figure()
+ax = Axis(fig[1,1],
+    xlabel = "Azimuthal curvature",
+    ylabel = "Elevation curvature",
+)
+
+
+hm = heatmap!(ax,azuCurvs,eleCurvs,tor)
+
+Colorbar(fig[1,2],hm,label = "Torsion")
+
+save("curvatureANDAngTorsionHEAT.png",fig)
+
+fig = Figure()
+ax = Axis(fig[1,1],
+    xlabel = "Azimuthal curvature",
+    ylabel = "Elevation curvature",
+)
+
+
+hm = heatmap!(ax,azuCurvs,eleCurvs,pol)
+
+Colorbar(fig[1,2],hm,label = "Polar Angle")
+
+save("curvatureANDAngPolarHEAT.png",fig)
+
+fig = Figure()
+ax = Axis(fig[1,1],
+    xlabel = "Azimuthal curvature",
+    ylabel = "Elevation curvature",
+)
+
+
+hm = heatmap!(ax,azuCurvs,eleCurvs,aziDir)
+
+Colorbar(fig[1,2],hm,label = "Azimuthal Direction")
+
+save("curvatureANDAngAzidirHEAT.png",fig)
